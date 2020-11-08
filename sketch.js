4ff77b9c3b43d5c2b82a7e9fc7945ecfa8eb9d36
nickbroeks <nick.broeks@live.nl>
2020-11-08 15:20:17 +0100

    Made some beginning

--- sketch.js
+++ sketch.js
@@ -1,11 +1,40 @@
 //js with p5 library
 //p5: https://p5js.org/
 
+const blockSize = 20;
+
+const width = 800 - (800 % blockSize);
+const height = 600 - (600 % blockSize);
+
+var snake;
+
+
 function setup() {
-    createCanvas(700, 700);
+    createCanvas(width, height);
+    let p1 = createVector(4,4);
+    let p2 = createVector(5,4);
+
+    console.log(p1);
+    snake = [p1 , p2];
 }
 
 function draw() {
-    background(100);
-    square(20,10,10,10);
+    background(0);
+    // for (let i = 0; i < height / blockSize; i++) {
+    //     for (let j = 0; j < width / blockSize; j++) {
+    //         noStroke();
+    //         fill(20, 200, 90);
+    //         drawSquare(j,i);
+    //     }
+    // }
+    noStroke();
+    fill(20, 200, 90);
+    for (const index in snake) {
+        let vector = snake[index];
+        drawSquare(vector.x, vector.y);
+    }
+}
+
+function drawSquare(x, y) {
+    square(x * blockSize, y * blockSize, blockSize - 2);
 }
